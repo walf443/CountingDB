59c9db00eb5375039e9e28b733623250151b73ad
Keiji, Yoshimi <walf443@gmail.com>
2011-11-05 14:37:23 +0900

    added server.log test.

---
+++ test/log.js
@@ -0,0 +1,38 @@
+
+require('../test_helper.js');
+var counting_db = require('counting_db/server');
+"use strict";
+
+QUnit.module("testing server.log", {});
+
+var mock_logger = function() {
+    return this;
+};
+mock_logger.prototype.message = "";
+mock_logger.prototype.log = function(message) {
+    this.message += message;
+};
+
+QUnit.test("in case log_level is debug", function() {
+    var server = new counting_db.server({
+        "logger": new mock_logger(),
+        "log_level": "debug",
+    });
+
+    var d = new Date(2011, 11, 5, 14, 32, 45);
+    server.log("debug", "foo bar", d);
+    QUnit.equal(server.logger.message, "[Mon Dec 05 2011 14:32:45 GMT+0900 (JST)][debug] foo bar", "should be logged");
+});
+
+QUnit.test("in case log_level is info", function() {
+    var server = new counting_db.server({
+        "logger": new mock_logger(),
+        "log_level": "info",
+    });
+
+    var d = new Date(2011, 11, 5, 14, 32, 45);
+    server.log("debug", "foo bar", d);
+    QUnit.equal(server.logger.message, "", "should not be logged");
+});
+
+QUnit.start();
---
+++ test_helper.js
@@ -0,0 +1,10 @@
+exports = module.exports = global;
+
+require.paths.push('lib/');
+QUnit = require("./node_modules/qunit-tap/vendor/qunit/qunit/qunit").QUnit;
+var qunitTap = require('qunit-tap').qunitTap;
+qunitTap(QUnit, require('sys').puts, { noPlan: true });
+QUnit.init();
+
+exports.assert = QUnit;
+
