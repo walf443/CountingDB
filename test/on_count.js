512db403689883b7646281135193677f0a3d6c04
Keiji, Yoshimi <walf443@gmail.com>
2011-11-06 15:42:58 +0900

    write on_count test.

---
+++ test/on_count.js
@@ -0,0 +1,39 @@
+
+var t = require('../test_helper.js');
+var counting_db = require('counting_db/server');
+"use strict";
+
+QUnit.module("testing server.on_count", {});
+
+QUnit.test("testing server.on_count", function() {
+    var server = new counting_db.server({
+        "log_level": "warn",
+    });
+
+    var client = new t.mock_client();
+    server.on_count(client, "12345", ["daily20111106", "weekly201130", "monthly201111"]);
+
+    QUnit.equal(client.writeBuffer, "OK\r\n", "response OK");
+    QUnit.equal(server.db["daily20111106"]["set_count"], 1, "set_count should be updated");
+    QUnit.equal(server.db["weekly201130"]["set_count"], 1, "set_count should be updated");
+    QUnit.equal(server.db["monthly201111"]["set_count"], 1, "set_count should be updated");
+
+    QUnit.equal(server.db["daily20111106"]["unique_count"], 1, "unique_count should be updated");
+    QUnit.equal(server.db["weekly201130"]["unique_count"], 1, "unique_count should be updated");
+    QUnit.equal(server.db["monthly201111"]["unique_count"], 1, "unique_count should be updated");
+
+    // retry
+    client.writeBuffer = "";
+    server.on_count(client, "12345", ["daily20111106", "weekly201130", "monthly201111"]);
+    QUnit.equal(client.writeBuffer, "OK\r\n", "response OK");
+
+    QUnit.equal(server.db["daily20111106"]["set_count"], 2, "set_count should be updated");
+    QUnit.equal(server.db["weekly201130"]["set_count"], 2, "set_count should be updated");
+    QUnit.equal(server.db["monthly201111"]["set_count"], 2, "set_count should be updated");
+
+    QUnit.equal(server.db["daily20111106"]["unique_count"], 1, "unique_count should not be updated");
+    QUnit.equal(server.db["weekly201130"]["unique_count"], 1, "unique_count should not be updated");
+    QUnit.equal(server.db["monthly201111"]["unique_count"], 1, "unique_count should not be updated");
+});
+
+QUnit.start();
